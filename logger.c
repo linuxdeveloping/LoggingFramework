8ad3e491c8b0f8a35e2c98af6c215ce00f7693c4
Kevin.s <kevinstiehl@rocketmail.com>
2014-10-24 10:10:53 +0200

    first comit

---
+++ logger.c
@@ -0,0 +1,10 @@
+#include "logger.h"
+
+
+void initLOGGER(char *a){
+if(a == NULL) {
+LOGLEVEL = getenv("LOGLEVEL");
+
+}else{
+}			
+}
---
+++ logger.h
@@ -0,0 +1,7 @@
+#ifndef LOGGER_MAIN_HEADER
+
+#define LOGGER_MAIN_HEADER
+char *loglevel = "INFO";
+void initLOGGER(char *a);
+
+#endif
