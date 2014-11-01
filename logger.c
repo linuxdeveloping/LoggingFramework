68fd746338b204d1f94292a11eca540e668c92eb
kevin <kevin@heisenberg.local>
2014-11-01 17:12:14 +0100

    Did some basic stuff

--- logger.c
+++ logger.c
@@ -1,10 +1,35 @@
 #include "logger.h"
 
-
-void initLOGGER(char *a){
-if(a == NULL) {
-LOGLEVEL = getenv("LOGLEVEL");
-
-}else{
-}			
+void initLOGGER(const char Loglevel, const char *filename){
+	char logger_level = Loglevel;
+	logger_file = filename;
 }
+
+void logDebug(const char *msg){
+	int fd = open(logger_file, O_APPEND);
+	if(fd < 0){
+		return;
+	}
+	ssize_t written = write(fd, msg, strlen(msg));
+	if(written < 0){
+		close(fd);
+		return;
+	}
+	close(fd);
+}
+void logInfo(const char *msg){
+
+}
+void logImportant(const char *msg){
+
+}
+void logWarn(const char *msg){
+
+}
+void logError(const char *msg){
+
+}
+void logWtf(const char *msg){
+
+}
+
--- logger.h
+++ logger.h
@@ -1,7 +1,28 @@
 #ifndef LOGGER_MAIN_HEADER
 
 #define LOGGER_MAIN_HEADER
-char *loglevel = "INFO";
-void initLOGGER(char *a);
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define LOG_DEBUG 1
+#define LOG_INFO 2
+#define LOG_IMPORTANT 3
+#define LOG_WARN 4
+#define LOG_ERROR 5
+#define LOG_WTF 6
+
+static const char logger_level;
+static const char *logger_file;
+void initLOGGER(const char Loglevel, const char *filename);
+
+
+void logDebug(const char *msg);
+void logInfo(const char *msg);
+void logImportant(const char *msg);
+void logWarn(const char *msg);
+void logError(const char *msg);
+void logWtf(const char *msg);
+
 
 #endif
